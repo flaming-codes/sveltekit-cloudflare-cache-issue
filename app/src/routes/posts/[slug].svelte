3948904a74f375d8a47ad15a8920d5723846d4cf
Tom Schönmann <11635736+flaming-codes@users.noreply.github.com>
2022-02-06 20:02:40 +0000

    wip add creds

--- app/src/routes/posts/[slug].svelte
+++ app/src/routes/posts/[slug].svelte
@@ -6,6 +6,7 @@ import type { Load } from "@sveltejs/kit/types";
     export const load : Load = async({fetch, params}) => {
        try {           
         const res = await fetch(`/posts/${params.slug}.json`, {
+            credentials: "omit",
             headers: {
                 "Cache-Control": "max-age=604800"
             }
